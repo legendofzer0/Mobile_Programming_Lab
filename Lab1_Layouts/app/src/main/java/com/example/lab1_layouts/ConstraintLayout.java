c166f5eb1b336c8692e8f2558063dce1bbbcd4e1
Jenish Maharjan <98453046+legendofzer0@users.noreply.github.com>
2025-06-02 17:17:32 +0545

    Layout Done

---
+++ Lab1_Layouts/app/src/main/java/com/example/lab1_layouts/ConstraintLayout.java
@@ -0,0 +1,18 @@
+package com.example.lab1_layouts;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
--- Lab1_Layouts/app/src/main/java/com/example/lab1_layouts/TableLayout.java
+++ Lab1_Layouts/app/src/main/java/com/example/lab1_layouts/TableLayout.java
@@ -3,11 +3,11 @@ package com.example.lab1_layouts;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RelativeLayout extends AppCompatActivity {
+public class TableLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relative_layout);
+        setContentView(R.layout.activity_table_layout);
     }
 }
