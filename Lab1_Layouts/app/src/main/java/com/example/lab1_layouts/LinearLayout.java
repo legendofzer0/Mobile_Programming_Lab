ab455ef965685803244f8619713f8fae71edc903
Jenish Maharjan <98453046+legendofzer0@users.noreply.github.com>
2025-06-02 16:24:25 +0545

    Mobile Programing Lab

---
+++ Lab1_Layouts/app/src/main/java/com/example/lab1_layouts/LinearLayout.java
@@ -0,0 +1,13 @@
+package com.example.lab1_layouts;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
